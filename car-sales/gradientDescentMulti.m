aab2c6fa48439ed1af63f8ad47f78d53b67bee1f
Aaron D'souza <aaron.dsouza@honeywell.com>
2018-11-05 20:01:14 +0530

    Implemented gradient multiple

--- car-sales/gradientDescentMulti.m
+++ car-sales/gradientDescentMulti.m
@@ -16,7 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    theta0 = theta(1) - ( alpha /m ) * sum(((X * theta) - y)); 
+    theta1 = theta(2) - ( alpha /m ) * sum(((X * theta) - y) .* X(:,2));
+    theta2 = theta(3) - ( alpha /m ) * sum(((X * theta) - y) .* X(:,3));
+    theta = [theta0; theta1; theta2];
+    
+    J_history(iter) = computeCost(X, y, theta); % J_history(iter) is (1,1)
+end
 
 
 
